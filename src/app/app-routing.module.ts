1db2a82e0d53ae18acbdfb113a4e9544a52daea6
sheepreak <mathieudalle93@gmail.com>
2018-10-09 16:44:29 +0200

    auth started, unfinished and buggy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {FilmListComponent} from './film-list/film-list.component';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './user-list/user-list.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   { path: 'films', component: FilmListComponent },
   { path: 'users', component: UserListComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FilmService} from './shared/film/film.service';
 import {FilmListComponent} from './film-list/film-list.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {UserListComponent} from './user-list/user-list.component';
 import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {JwtInterceptor} from './_helpers/jwt.interceptor';
+import {ErrorInterceptor} from './_helpers/error.interceptor';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmListComponent,
     UserListComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +30,12 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [FilmService],
+  providers: [FilmService,{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
